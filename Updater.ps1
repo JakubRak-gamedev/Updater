65da7ff00e49b17dae8470c9a4b56127d9f61bfd
Jakub Rak <kubarak326@gmail.com>
2025-02-07 11:53:48 +0100

    test

--- Updater.ps1
+++ Updater.ps1
@@ -1,3 +1,81 @@
+function Confirm-ProxyServer {
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $TargetUri
+    )
+
+    Write-Verbose "Calling $($MyInvocation.MyCommand)"
+    try {
+        $proxyObject = ([System.Net.WebRequest]::GetSystemWebProxy()).GetProxy($TargetUri)
+        if ($TargetUri -ne $proxyObject.OriginalString) {
+            Write-Verbose "Proxy server configuration detected"
+            Write-Verbose $proxyObject.OriginalString
+            return $true
+        } else {
+            Write-Verbose "No proxy server configuration detected"
+            return $false
+        }
+    } catch {
+        Write-Verbose "Unable to check for proxy server configuration"
+        return $false
+    }
+}
+
+function Invoke-WebRequestWithProxyDetection {
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    param (
+        [Parameter(Mandatory = $true, ParameterSetName = "Default")]
+        [string]
+        $Uri,
+
+        [Parameter(Mandatory = $false, ParameterSetName = "Default")]
+        [switch]
+        $UseBasicParsing,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "ParametersObject")]
+        [hashtable]
+        $ParametersObject,
+
+        [Parameter(Mandatory = $false, ParameterSetName = "Default")]
+        [string]
+        $OutFile
+    )
+
+    Write-Verbose "Calling $($MyInvocation.MyCommand)"
+    if ([System.String]::IsNullOrEmpty($Uri)) {
+        $Uri = $ParametersObject.Uri
+    }
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    if (Confirm-ProxyServer -TargetUri $Uri) {
+        $webClient = New-Object System.Net.WebClient
+        $webClient.Headers.Add("User-Agent", "PowerShell")
+        $webClient.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+    }
+
+    if ($null -eq $ParametersObject) {
+        $params = @{
+            Uri     = $Uri
+            OutFile = $OutFile
+        }
+
+        if ($UseBasicParsing) {
+            $params.UseBasicParsing = $true
+        }
+    } else {
+        $params = $ParametersObject
+    }
+
+    try {
+        Invoke-WebRequest @params
+    } catch {
+        Write-VerboseErrorInformation
+    }
+}
+
 function Get-ScriptUpdateAvailable {
     [CmdletBinding()]
     [OutputType([PSCustomObject])]
@@ -21,7 +99,7 @@ function Get-ScriptUpdateAvailable {
         Error          = $null
     }
 
-    if ((Get-AuthenticodeSignature -FilePath $scriptFullName).Status -eq "NotSigned") {
+    if ((Get-AuthenticodeSignature -FilePath $scriptFullName).Status -eq "Signed") {
         Write-Warning "This script appears to be an unsigned test build. Skipping version check."
     } else {
         try {
