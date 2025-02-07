f634f9909b5fcfb563177f1543023773555f316c
Jakub Rak <kubarak326@gmail.com>
2025-02-07 11:38:30 +0100

    updater testing

---
+++ Updater.ps1
@@ -0,0 +1,48 @@
+function Get-ScriptUpdateAvailable {
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]
+        $VersionsUrl = "https://github.com/JakubRak-gamedev/Updater/blob/main/Version.csv"
+    )
+
+    $BuildVersion = "0.0.0"
+
+    $scriptName = $script:MyInvocation.MyCommand.Name
+    $scriptPath = [IO.Path]::GetDirectoryName($script:MyInvocation.MyCommand.Path)
+    $scriptFullName = (Join-Path $scriptPath $scriptName)
+
+    $result = [PSCustomObject]@{
+        ScriptName     = $scriptName
+        CurrentVersion = $BuildVersion
+        LatestVersion  = ""
+        UpdateFound    = $false
+        Error          = $null
+    }
+
+    if ((Get-AuthenticodeSignature -FilePath $scriptFullName).Status -eq "NotSigned") {
+        Write-Warning "This script appears to be an unsigned test build. Skipping version check."
+    } else {
+        try {
+            $versionData = [Text.Encoding]::UTF8.GetString((Invoke-WebRequestWithProxyDetection -Uri $VersionsUrl -UseBasicParsing).Content) | ConvertFrom-Csv
+            $latestVersion = ($versionData | Where-Object { $_.File -eq $scriptName }).Version
+            $result.LatestVersion = $latestVersion
+            if ($null -ne $latestVersion) {
+                $result.UpdateFound = ($latestVersion -ne $BuildVersion)
+            } else {
+                Write-Warning ("Unable to check for a script update as no script with the same name was found." +
+                    "`r`nThis can happen if the script has been renamed. Please check manually if there is a newer version of the script.")
+            }
+
+            Write-Verbose "Current version: $($result.CurrentVersion) Latest version: $($result.LatestVersion) Update found: $($result.UpdateFound)"
+        } catch {
+            Write-Verbose "Unable to check for updates: $($_.Exception)"
+            $result.Error = $_
+        }
+    }
+
+    return $result
+}
+
+Get-ScriptUpdateAvailable
