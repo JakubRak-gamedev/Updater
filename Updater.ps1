7b4f69adbd4aca64960dffc2c95218e7980d8c7d
Jakub Rak <kubarak326@gmail.com>
2025-02-13 14:18:18 +0100

    prawie gotowa baza

--- Updater.ps1
+++ Updater.ps1
@@ -160,7 +160,7 @@ function Get-ScriptUpdateAvailable {
     }
 
     if ((Get-AuthenticodeSignature -FilePath $scriptFullName).Status -eq "NotSigned") {
-        Write-Warning "This script appears to be an unsigned test build. Skipping version check."
+        Write-Yellow "This script appears to be an unsigned test build. Skipping version check."
     } else {
         try {
             $versionData = [Text.Encoding]::UTF8.GetString((Invoke-WebRequestWithProxyDetection -Uri $VersionsUrl -UseBasicParsing).Content) | ConvertFrom-Csv
@@ -171,7 +171,7 @@ function Get-ScriptUpdateAvailable {
             if ($null -ne $latestVersion) {
                 $result.UpdateFound = ($latestVersion -ne $ScriptVersion)
             } else {
-                Write-Warning ("Unable to check for a script update as no script with the same name was found." +
+                Write-Yellow ("Unable to check for a script update as no script with the same name was found." +
                     "`r`nThis can happen if the script has been renamed. Please check manually if there is a newer version of the script.")
             }
 
@@ -202,7 +202,7 @@ function Confirm-Signature {
         $sig = Get-AuthenticodeSignature -FilePath $File
 
         if ($sig.Status -ne 'Valid') {
-            Write-Warning "Signature is not trusted by machine as Valid, status: $($sig.Status)."
+            Write-Yellow "Signature is not trusted by machine as Valid, status: $($sig.Status)."
             throw
         }
 
@@ -210,24 +210,24 @@ function Confirm-Signature {
         $chain.ChainPolicy.VerificationFlags = "IgnoreNotTimeValid"
 
         if (-not $chain.Build($sig.SignerCertificate)) {
-            Write-Warning "Signer certificate doesn't chain correctly."
+            Write-Yellow "Signer certificate doesn't chain correctly."
             throw
         }
 
         if ($chain.ChainElements.Count -le 1) {
-            Write-Warning "Certificate Chain shorter than expected."
+            Write-Yellow "Certificate Chain shorter than expected."
             throw
         }
 
         $rootCert = $chain.ChainElements[$chain.ChainElements.Count - 1]
 
         if ($rootCert.Certificate.Subject -ne $rootCert.Certificate.Issuer) {
-            Write-Warning "Top-level certificate in chain is not a root certificate."
+            Write-Yellow "Top-level certificate in chain is not a root certificate."
             throw
         }
 
         if ($rootCert.Certificate.Subject -ne $MicrosoftSigningRoot2010 -and $rootCert.Certificate.Subject -ne $MicrosoftSigningRoot2011) {
-            Write-Warning "Unexpected root cert. Expected $MicrosoftSigningRoot2010 or $MicrosoftSigningRoot2011, but found $($rootCert.Certificate.Subject)."
+            Write-Yellow "Unexpected root cert. Expected $MicrosoftSigningRoot2010 or $MicrosoftSigningRoot2011, but found $($rootCert.Certificate.Subject)."
             throw
         }
 
@@ -275,33 +275,33 @@ function Invoke-ScriptUpdate {
         try {
             Invoke-WebRequestWithProxyDetection -Uri "$ScriptUrl/$scriptName" -OutFile $tempFullName
         } catch {
-            Write-Warning "AutoUpdate: Failed to download update: $($_.Exception.Message)"
+            Write-Yellow "AutoUpdate: Failed to download update: $($_.Exception.Message)"
             return $false
         }
         try {
             $tempFileHash = (Get-FileHash -Path $tempFullName -Algorithm SHA256).Hash
             if ($VersionHash -eq $tempFileHash) {
-                Write-Host "AutoUpdate: File hash validated."
+                Write-Grey "AutoUpdate: File hash validated."
                 if (Confirm-Signature -File $tempFullName) {
-                    Write-Host "AutoUpdate: Signature validated."
+                    Write-Grey "AutoUpdate: Signature validated."
                     if (Test-Path $oldFullName) {
                         Remove-Item $oldFullName -Force -Confirm:$false -ErrorAction Stop
                     }
                     Move-Item $scriptFullName $oldFullName
                     Move-Item $tempFullName $scriptFullName
                     Remove-Item $oldFullName -Force -Confirm:$false -ErrorAction Stop
-                    Write-Host "AutoUpdate: Succeeded."
+                    Write-Grey "AutoUpdate: Succeeded."
                     return $true
                 } else {
-                Write-Warning "AutoUpdate: Signature could not be verified: $tempFullName."
-                Write-Warning "AutoUpdate: Update was not applied."
+                Write-Yellow "AutoUpdate: Signature could not be verified: $tempFullName."
+                Write-Yellow "AutoUpdate: Update was not applied."
                 }
             } else {
-                Write-Warning "AutoUpdate: File hash is not valid: $tempFullName."
-                Write-Warning "AutoUpdate: Update was not applied."
+                Write-Yellow "AutoUpdate: File hash is not valid: $tempFullName."
+                Write-Yellow "AutoUpdate: Update was not applied."
             }
         } catch {
-            Write-Warning "AutoUpdate: Failed to apply update: $($_.Exception.Message)"
+            Write-Yellow "AutoUpdate: Failed to apply update: $($_.Exception.Message)"
         }
     }
 
@@ -335,7 +335,7 @@ function Test-ScriptVersion {
         if ($AutoUpdate) {
             return Invoke-ScriptUpdate -VersionHash $updateInfo.LatestHash
         } else {
-            Write-Warning "$($updateInfo.ScriptName) $ScriptVersion is outdated. Please download the latest, version $($updateInfo.LatestVersion)."
+            Write-Yellow "$($updateInfo.ScriptName) $ScriptVersion is outdated. Please download the latest, version $($updateInfo.LatestVersion)."
         }
     }
 
@@ -368,6 +368,16 @@ function New-LoggerInstance {
     $Script:OutputFullPath = Join-Path -Path $Script:OutputFilePath -ChildPath ('{0}{1}' -f $FileName, $endName)
 }
 
+function Write-Verbose {
+    param(
+        [Parameter(Position = 1, ValueFromPipeline)]
+        [string]$Message
+    )
+    Microsoft.PowerShell.Utility\Write-Verbose $Message
+    if ($Script:VerboseEnabled) {
+        Write-HostLog "VERBOSE: $Message"
+    }
+}
 
     $scriptFileName = $script:MyInvocation.MyCommand.Name
 
@@ -389,7 +399,7 @@ function New-LoggerInstance {
 } process {
 
     if (-not (Confirm-Administrator) -and (-not $AnalyzeDataOnly -and -not $ScriptUpdateOnly)) {
-        Write-Warning "The script needs to be executed in elevated mode. Starting the script as an Administrator."
+        Write-Yellow "The script needs to be executed in elevated mode. Starting the script as an Administrator."
         if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
             $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
             Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
@@ -413,3 +423,80 @@ function New-LoggerInstance {
     }
 
 }
+
+# SIG # Begin signature block
+# MIINxAYJKoZIhvcNAQcCoIINtTCCDbECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUqIdMa6+BUtZ4sxBR6N6CP38A
+# 1oWgggs+MIIFkTCCA3mgAwIBAgIUXLFVzgd31jXC7h7dxgMcN8IB4rUwDQYJKoZI
+# hvcNAQELBQAwNzELMAkGA1UEBhMCUEsxEDAOBgNVBAoTB0NvZGVnaWMxFjAUBgNV
+# BAMTDUNvZGVnaWMgQ0EgRzIwHhcNMjUwMjExMTEzNzIzWhcNMjUwNDEyMTAzNzIz
+# WjBOMRwwGgYJKoZIhvcNAQkBFg1qYWt1YkBzaWl0LnBsMRIwEAYDVQQDEwlKYWt1
+# YiBSYWsxDTALBgNVBAoTBFNJSVQxCzAJBgNVBAYTAlBMMIIBIjANBgkqhkiG9w0B
+# AQEFAAOCAQ8AMIIBCgKCAQEAxgEeGDCXhGqqxa9U70bI8OQc0e0fQHL6rJEikYVU
+# Lino02O5UuX2Z2tfoxyqs0QtoDGr1rUzuyXw5wTC2AJPbMN8kYbI3/FYBW8tggAr
+# Or+2kaqWOmUf+dsXN0I7shcGY+32E1L2S6xQhpPNU90SHydH5GUBeMdbjRRwHK3v
+# ePjACCo+NMhYEl/M7iX50C1mAi1KgI1dKi1Uslmykdlu8+oByr4w0VjfGnoiaB5W
+# H3jEOrzoZTWiFm5ZMjq8YSQsk9iEHEGPmGXOtzOKN0URU9dwxxXlJP2JfvYvIhAH
+# 5/a9MTrSlEHzjLF0nWwXezra24wdQXzUulUrE7gub2AawQIDAQABo4IBfDCCAXgw
+# FgYDVR0lAQH/BAwwCgYIKwYBBQUHAwMwDgYDVR0PAQH/BAQDAgeAMB8GA1UdIwQY
+# MBaAFJXrz/R5EzTy3VAeQXtrcU5Q6BAIMIHFBgNVHSAEgb0wgbowgbcGCSsGAQQB
+# g6hkATCBqTCBpgYIKwYBBQUHAgIwgZkMgZZBcyBwZXIgdGhpcyBwb2xpY3kgaWRl
+# bnRpdHkgb2YgdGhlIHN1YmplY3QgaXMgbm90IGlkZW50aWZpZWQuIFlvdSBtdXN0
+# IG5vdCB1c2UgdGhlc2UgY2VydGlmaWNhdGVzIGZvciB2YWx1YWJsZSB0cmFuc2Fj
+# dGlvbnMuIE5PIExJQUJJTElUWSBJUyBBQ0NFUFRFRC4wCQYDVR0TBAIwADA7BgNV
+# HR8ENDAyMDCgLqAshipodHRwczovL3BraS5jb2RlZ2ljLmNvbS9jcmxzL0NvZGVn
+# aWNDQS5jcmwwHQYDVR0OBBYEFN4Q5d8nRYcB+4HTGEoVL/Oz9u2/MA0GCSqGSIb3
+# DQEBCwUAA4ICAQBdalQdCfje9KzDn0UKQLt3zploRqhk3f42pyDmR+jfFFdg0Ueh
+# WwlkaxZyvdeUffQw3OYnQjGjTfUDP1AcvCNxmRjgx4uv/NTTmG6f7mA8xqTz6ukb
+# oTHR5EWaui44pTwOCOJ4CF6SOWztAYPm6Z6k5QGSE6CjsqTsRPHWXACJRjPKCcsD
+# L5mlbEO6PmD2+7iDx3WJ3s7FZg5SgR0RezrcR/SQhXMc4/ue/5oOq8R0grHKjocx
+# pCu2VO8Y1BvEi02S/U+LnwOQtzgFySInTfyNwS4WsKJlwmR5ofd/2xgrXjF4VFk/
+# G1IBuWMuqVlxN0tpU4HSsXrzaOrsNOoYGS0oQ4FVNGbvglIFVcYzcKcHzsI2iumV
+# Vl8iiSJA7o5Q1Ifh2P2j1JGDySJzHGgdQn0osMvZ3ef6A3yHP/uS6Gi9L7zffZJK
+# JRdY0Yj6pgqWopcKN8XJavdcJbIEIYvVqTUhlFgVDYWRt1N6Owb2/iTKCbN/iueY
+# rTYlUNwLVvgz/nxzZUPVXrmZBE0xUAG7JL77tXWZz0SigDHQ43LOShWdhpgZBWMC
+# 88gyltD/kt0TbmSxEXfna6ZEeSDFmXhXJYgNQ1snh/BS2DV5cnjXDI1Ac+ukRE2V
+# nRoCjtXFggKqNY+VsIY3US4Obdg/gmLHzAAeckYYtbOiTrbLfpGv7cOiRzCCBaUw
+# ggONoAMCAQICFDowdofT+XUnwL7ki+mCzPL9Duc/MA0GCSqGSIb3DQEBDQUAMDwx
+# CzAJBgNVBAYTAlBLMRAwDgYDVQQKEwdDb2RlZ2ljMRswGQYDVQQDExJDb2RlZ2lj
+# IFJvb3QgQ0EgRzIwHhcNMjExMjA3MTAxOTU2WhcNNDkxMjA3MTAxOTU1WjA3MQsw
+# CQYDVQQGEwJQSzEQMA4GA1UEChMHQ29kZWdpYzEWMBQGA1UEAxMNQ29kZWdpYyBD
+# QSBHMjCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAIxdsKkzlHrRPy58
+# fVGka9JnRAC7Xb5Mn19MHMsCEz5ptVvdAJVFI3E1sVRR7C/jh4Wb2EE6+ngv1ABJ
+# dNFjKQjUJatIBn/5TARPPQ4XZH4wvxlf+sEXQYts1A484nEjkkSTtsLE43NMZKPO
+# jME/BHhvVi2i47t3Z6+SnIT6ucxXp5w7fIKHk5SpPCd4+Lq9EXI8PbmeL6MOmPQk
+# w3925dMlkIBG7puElq/neVlOwA23sH8tJlTtHdqzOfch8+dAcj4S906ty7egsneg
+# vHJ2iGY4ewqIfzuWK0Ym6NSxMXrmbBpgGBYPzFGTijSkqs9g1m7kzomkuK2hUNjP
+# +4QjUBNnxm9qBpVdIrw26kYk8AuJa0GiTTpCxGs+Tt/L48961UPvQnwzMQLTYEG6
+# gFZHQ2SLsAfEH7v4mbdhv1GHvv82t70MYpQZel/hCOaBzabaVV0qWf9vd7BLseq5
+# HU9rp358G57KLo8Sg7auRSt5yx1Uu3u3uJFHq2XzZVv0IiZxttcHt9ZH2vqiSvFz
+# KlD0WLiorF331XRf24zS0eOzGP1PNmSnNlMeKpW/mopsyGo/olGYjdm/qeUHqsTM
+# fnm1oRm4IENfgfur/I+O8kbRnhJVUG+K4UcZnMgWH3gCtF0VbVE8sTSFkocjymlc
+# ZI7UDdX3YsZ/brogDgPTjl3YUoDhAgMBAAGjgaMwgaAwDwYDVR0TAQH/BAUwAwEB
+# /zAOBgNVHQ8BAf8EBAMCAQYwHwYDVR0jBBgwFoAUsL8sJ62K4SFQtz1Rq8JX5g0W
+# ezowPQYDVR0fBDYwNDAyoDCgLoYsaHR0cHM6Ly9wa2kuY29kZWdpYy5jb20vY3Js
+# cy9Db2RlZ2ljUm9vdC5jcmwwHQYDVR0OBBYEFJXrz/R5EzTy3VAeQXtrcU5Q6BAI
+# MA0GCSqGSIb3DQEBDQUAA4ICAQANG37Y2KcjlJZ7Roc9hnJX5Sl+gklV0ziUwSAs
+# Vzd3Zh3RNYnL0nAbQxirWzrSyDMy9pYILONejb/CU9EBGunFUP5W3IUjaX1uQcgg
+# QcS8si0VbEMrX3WpzrRCLQveey95rqjwsSlUZLP+pYEqf+ssI9fPziHP8PdvbCen
+# t8MxiFwiz0A0pWFioyk53dcua4bRFwHGzd0PdkmCH0KdsC17GWFfM1bdn3SLapQH
+# Wzv3B+fl18xSYzGeDdTO4DIGOxRRUEJBRp7qtNr/lfYuotruPjm59h/NmzMsQ9NJ
+# 2mJDcHObL69EiKFaUdE4bTHuEczaiLkSlHQKSekMYxDeSVfanbhbqn2VEbxu7JJV
+# r1XM9CC2H3Kjx18QnULztQMsJB+UViwhX2Qr8y7jEYnqDWSBbtHNl7ua78pJd2QE
+# FMYO6RNLMiSTWSup5M5wwBsEbCABVv53HIl1LM4eXVTINdXyd6YIFvrpKuvfQ9vZ
+# x9HblPk8XKhie0Ja+xKcqrWc+6OK86DXm8X4qOrjklniX9m1cKxM3RerFLPWZXxF
+# va/xCKcWbJmXyU9PtvTMRAJdvPVQeNsNeCC5Tr0iDARfuMT+o3YMtEfRx9AZPtBR
+# wEHUPF4N6b7vxrXZFz6MlDLYc8SMCBYzdPmI5/xtSQjFuOaE+XolwfQpq9uuXXnV
+# /pcuwTGCAfAwggHsAgEBME8wNzELMAkGA1UEBhMCUEsxEDAOBgNVBAoTB0NvZGVn
+# aWMxFjAUBgNVBAMTDUNvZGVnaWMgQ0EgRzICFFyxVc4Hd9Y1wu4e3cYDHDfCAeK1
+# MAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3
+# DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEV
+# MCMGCSqGSIb3DQEJBDEWBBSeGVER7oxJ8QOSgJ3Jglc+HVOy9jANBgkqhkiG9w0B
+# AQEFAASCAQCJT/H/PWuVZPKdB+ZwHhWf8Q4a1/nDi0Q6Vd97gAO5Zai5CsX1LSBl
+# PTwYxPED6LJOWaR+8FeO8cxvG9qd4mL70YXOkQUXSLSSF1QkKIQjXA3caqP+Mu9M
+# IZjwK76p5KtTUIZyibaifn+ffErkY3pM8DbF2/Yx2I4Ql3fstrTh0SJRxkEMdr0S
+# QY4PU/bhzJnivvhIkWp+6uBW8SkX+7SlVNwYuJcPPBg08uMW4Gwyus3GLlFxBExe
+# Q7GkWIBYGU8BbL+IaIqss3CvSRuyNR1ykvWg9WPeYWQrzBD+l9UNCl4K8+psZZ8E
+# PJP0HHzsObKGmhzr+mpwIF3hdC7zhJ7g
+# SIG # End signature block
